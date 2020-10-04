83d98f00990df417a4318e5d167accc63d623cf1
Timo Suomela <timo.suomela@reaktor.com>
2020-10-04 17:58:25 +0300

    Fail fast if docker is not installed or not running

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@ readonly repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function main {
   cd "$repo"
+
+  command -v docker > /dev/null 2>&1 || { echo >&2 "I require docker but it's not installed. Aborting."; exit 1; }
+  docker ps > /dev/null 2>&1 || { echo >&2 "Running 'docker ps' failed. Is docker daemon running? Aborting."; exit 1; }
+
   docker-compose up -d
 
   export NVM_DIR="${NVM_DIR:-$HOME/.cache/nvm}"
